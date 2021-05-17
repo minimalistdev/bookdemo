4e3813a151a2b3e128a03b83fb2c9d1eb80370af
minimalistdev <cfs.bsi@outlook.com>
2021-05-17 20:27:29 +0100

    clean the code

--- src/main/java/com/minimalistdev/bookdemo/repository/BookRepository.java
+++ src/main/java/com/minimalistdev/bookdemo/repository/BookRepository.java
@@ -7,10 +7,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
-
 @RepositoryRestResource(collectionResourceRel = "book", path = "books")
 public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
-//    List<Book> findByPriceGreaterThan(@Param("price") Long price);
     Page<Book> findByPriceGreaterThan(@Param("price") Long price, Pageable pageable);
 }
